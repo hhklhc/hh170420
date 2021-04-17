d1e223b50691d4c68a6a043d7fd9e5c9f8945144
hhklhc <hoangvanha1996@outlook.com>
2021-04-17 16:11:43 +0700

    lamlaibaitapwebfrontend13

---
+++ HOCLAI/BaiTaplamlai/Exercise-Airthmetic-operators/js.js
@@ -0,0 +1,66 @@
+// Bài 1: Viết chương trình nhập 2 số a, b, tính và in ra kết quả tất cả phép tính cơ bản với 2 số đó
+
+
+// let a = Number(prompt("Nhập số thứ nhất vào đi bạn ?"));
+// let b = Number(prompt("Nhập số thứ hai nữa ^__^ ?"));
+// function cong(a, b) {
+//     alert("a + b = " + (a + b));
+//     return "a + b = " + (a + b);
+// }
+// console.log(cong(a, b));
+
+// function tru(a, b) {
+//     alert("a - b = " + (a - b));
+//     return "a - b = " + (a - b);
+// }
+// console.log(tru(a, b));
+
+// function nhan(a, b) {
+//     alert("a nhân b = " + (a * b));
+//     return "a nhân b = " + (a * b);
+// }
+// console.log(nhan(a, b));
+
+// function chia(a, b) {
+//     alert("a chia b = " + (a / b));
+//     return "a chia b = " + (a / b);
+// }
+// console.log(chia(a, b));
+
+
+// Bài 2: Viết chương trình nhập vào chiều dài, chiều rộng của hình chữ nhật, tính và in ra chu vi, 
+// diện tích của hình chữ nhật đó
+
+
+// let a = Number(prompt("Nhập vào chièu dài đi ?"));
+// let b = Number(prompt("Nhập chiều rộng nữa nhé ?"));
+// function chuVi(a, b) {
+//     alert("Chu vi hình chữ nhật là " + ((a + b) * 2));
+//     return "Chu vi hình chữ nhật là " + ((a + b) * 2);
+// }
+// console.log(chuVi(a, b));
+
+// function dienTich(a, b) {
+//     alert("Diện tích hình chữ nhật là " + (a * b));
+//     return "Diện tichs hình chữ nhật là " + (a * b);
+// }
+// console.log(dienTich(a, b));
+
+
+// Bài 3: Viết chương trình nhập vào bán kính hình tròn, tính và in ra chu vi, diện tích của hình tròn
+
+// let r = Number(prompt("Nhập vào bán kính hình tròn nhé ?"));
+
+// function chuVi(r) {
+//     alert("Chu vi hình tròn là " + (2 * 3.14 * r));
+//     return "Chu vi hình tròn là " + (2 * 3.14 * r);
+// }
+// console.log(chuVi(r));
+
+// function dienTich(r) {
+//     alert("Diện tích hình tròn là " + (r * r * 3.14));
+//     return "Diện tích hình tròn là " + (r * r * 3.14);
+// }
+// console.log(dienTich(r));
+
+
